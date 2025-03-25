605045a170c7fd93a0115fda1a557bee7e118507
kaleabtesfaye01 <kaleabtesfaye01@gmail.com>
2025-03-24 21:35:53 -0400

    dockerize frontend and backend for local dev.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,12 @@
+declare global {
+  interface Window {
+    env: {
+      API_URL: string;
+    };
+  }
+}
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5215/api'
+  apiUrl: window.env?.API_URL || 'http://localhost:5215/api'
 }; 
